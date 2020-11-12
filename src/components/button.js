8e397421b32c8dc1aaceef63aa225c9fd6e66d38
wang <w.hanseul@gmail.com>
2020-11-12 16:12:15 +0900

    feat: add value on click button

--- src/components/button.js
+++ src/components/button.js
@@ -3,26 +3,35 @@ import PropTypes from "prop-types"
 
 import "../styles/global.css"
 
+class Button extends React.Component {
+    state = {
+        answer: ''
+    };
+    
+  add = () => {
+    this.setState(current => ({ answer: current.answer + this.props.value }));
+  };
+ 
+  render() {
+    return (
+      <div>
+          <button onClick={this.add}>{this.state.answer}{this.props.text}</button>
+      </div>
+    );
+  }
 
-function Button({ text }) {
+};
 
-    const state = {
-        count: 0
-    }
-    const add = () => {
-        // this.setState = 1;
-    }
-
-  return (
-    <div>
-          <button onClick={add}>{state.count}{text}</button>
-    </div>
-  )
-}
-
+Button.defaultProps = {
+    text: 'Default text',
+    value:'Default value',
+};
 
 Button.propTypes = {
-  text: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
 }
+
+
 
 export default Button
