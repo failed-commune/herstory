2c632069bade9a6ecda6c2ec9fbe169f423f0e8b
wang <w.hanseul@gmail.com>
2020-11-14 01:08:50 +0900

    feat: switching cards and tossing props to next componets

--- src/components/card.js
+++ src/components/card.js
@@ -1,51 +1,60 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Button from "./button.js"
-
+import React, {useEffect, useState} from "react"
 import "./card.css"
 import "../styles/global.css"
+import { Link } from "gatsby"
 
-const data = [
-    {
-        question: "",
-        choice : {
-            text: "",
-            value:""            
+const Card = ({}) => {
+    //질문 답변 세트
+    const data = [
+        {
+            text: '오랜만에 약속 없는 주말, 하루를 어떻게 보낼까요?',
+            answer1: '드라마 정주행하기',
+            answer2: '친구를 불러내 요즘 힙하다는 카페 가기'
         },
-        choice : {
-            text: "",
-            value:"",
+        {
+            text: '길을 가다 고양이를 만난 당신, 어떻게 하나요?',
+            answer1: '음 귀엽네 하고 갈 길을 간다',
+            answer2: '근처에서 고양이 간식을 사서 준다',
+        },
+        {
+            text: '큰 맘 먹고 등산을 하러왔습니다. 정상은 멀기만 하고 내가 왜 그랬을까 후회되는 순간, 당신의 선택은?',
+            answer1: '일단 먹고 생각하자! 도시락부터 까먹는다',
+            answer2: '이왕 온 김에 정상은 보고 가야지, 힘내서 올라가본다',
+        },
+        {
+            text: '사람들이 가득한 회의실에서 나도 모르게 방구가 나왔습니다. 모른 척 하려는데 이게 웬걸 거름 냄새가 나기 시작합니다. 이 때 당신의 대처방법은?',
+            answer1: '제가 시원하게 한번 뀌었습니다~ 환기 한 번 할까요? - 들키기 전에 자진납세',
+            answer2: '윽 냄새.. 누구야? 필살의 포커페이스로 맞선다',
+        },
+    ]
+
+    //사용자 답변 모음 state
+    const [answer, setAnswer] = useState('');
+    const [cardIndex, setCardIndex] = useState(0);
+
+
+    useEffect(() => {
+        if (cardIndex == 3)
+            console.log("last index");
+            
+    }, [cardIndex]);
+
+    function add(value) {
+        setAnswer(answer + value);
+        if (cardIndex < 4) {
+            setCardIndex(cardIndex + 1);
         }
-    }
-]
+    };
 
-class Card extends React.Component{
-
-    turnCard = () => {
-
-    }
-
-    render() {
-        return (
-            <div>
-                <h2>{this.props.text}</h2>
-                <Button text={this.props.choice1} value='a' />
-                <Button text={this.props.choice2} value='b' />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h2>{answer}</h2>
+            {cardIndex === 4 ? <Link to= '/result/' state={ {props: answer} }>결과보기</Link> : <div><h2>{data[cardIndex].text}</h2>
+            <button onClick={() => add('a')}>{data[cardIndex].answer1}</button>
+            <button onClick={() => add('b')}>{data[cardIndex].answer2}</button></div>
 }
-
-Card.defaultProps = {
-    text: 'Default text',
-    choice1: 'choice1',
-    choice2: 'choice2',
-};
-
-Card.propTypes = {
-  text: PropTypes.string.isRequired,
-  choice1: PropTypes.string.isRequired,
-  choice2: PropTypes.string.isRequired,
+        </div>
+    );
 }
 
 export default Card
--- src/pages/page-1.js
+++ src/pages/page-1.js
@@ -1,35 +1,22 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Card from "../components/card"
 
 class Page extends React.Component{
-  state = {
-    answer: ''
-  };
 
-  add = () => {
-    this.setState(current => ({ answer: current.answer + this.props.value }));
-
-  };
 
   render() {
     return (
       <div>
           <Layout>
             <SEO title="Page one" />
-            <Card text="오랜만에 약속 없는 주말,
-              하루를 어떻게 보낼까요?" choice1="드라마 정주행" choice2="친구들과 힙스터 카페 가기"/>
+            <Card/>
           </Layout>
         </div>
     )
   }
 
-};
-Page.defaultProps = {
-    text: 'Default text',
-    value:'Default value',
 };
 
 
